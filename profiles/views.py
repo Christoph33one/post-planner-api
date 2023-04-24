80a13a084149c8e652741b335cbce78894ee9e33
Christoph33one <cjbrowning23@hotmail.com>
2023-04-24 10:00:27 +0000

    added custom permissions on all user profile. User can not access individual profiles

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from planner_api.permissions import IsOwnerOrReadOnly
 
 
 # Profile list view.
@@ -17,10 +18,12 @@ class ProfileList(APIView):
 # Profile detial view
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
