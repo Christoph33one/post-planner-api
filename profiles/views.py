aa59e194cba9226491d8661c0da5e8b1b5ff9462
Christoph33one <cjbrowning23@hotmail.com>
2023-04-24 10:27:20 +0000

    added login & logout views, IsOwner permissions & is_owner field to ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -11,7 +11,8 @@ from planner_api.permissions import IsOwnerOrReadOnly
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles, many=True, context={'request': request})
         return Response(serializer.data)
 
 
@@ -30,13 +31,16 @@ class ProfileDetail(APIView):
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile, context={'request': request})
         return Response(serializer.data)
 
     # PUT menthod
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={'request': request}
+            )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
