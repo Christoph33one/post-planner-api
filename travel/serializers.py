96dc1051f5cbec933c9059617a2aee1c6da54e51
Christoph33one <cjbrowning23@hotmail.com>
2023-04-25 09:44:48 +0000

    created TravelPlan serializer

---
+++ travel/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import TravelPlan
+
+
+class TravelPlanSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = TravelPlan
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'title',
+            'description', 'location', 'activties',
+            'image', 'image_filter'
+        ]
