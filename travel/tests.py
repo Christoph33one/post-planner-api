7ff939edcb0d58a455e19425c684284136ef2f97
Christoph33one <cjbrowning23@hotmail.com>
2023-05-02 17:46:37 +0000

    tested TravelPlanPostDetail valid user, invaild user, upadate own post and can not updte own post

--- travel/tests.py
+++ travel/tests.py
@@ -26,3 +26,36 @@ class TravelPlanPostViewList(APITestCase):
     def test_user_not_logged_in_cant_create_post(self):
         response = self.client.post('/travelplanposts/', {'title': 'a title', 'description': 'test', 'location': 'test location'} )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class TravelPlanPostDetailViewTest(APITestCase):
+    def setUp(self):
+        chris = User.objects.create_user(username='chris', password='pass')
+        brian = User.objects.create_user(username='brian', password='pass')
+        TravelPlan.objects.create(
+            owner=chris, title='a title', description='chris description'
+        )
+        TravelPlan.objects.create(
+            owner=chris, title='a title', description='chris description'
+        )
+
+    def test_can_retrieve_post_using_valid_id(self):
+        response = self.client.get('/posts/1/')
+        self.assertEqual(response.data['title'], 'a title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cant_retrieve_post_using_invalid_id(self):
+        response = self.client.get('/posts/999/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_update_own_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/1/', {'title': 'a new title', 'description': 'testing', 'location': 'testing location'})
+        post = TravelPlan.objects.filter(pk=1).first()
+        self.assertEqual(post.title, 'a new title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='adam', password='pass')
+        response = self.client.put('/posts/2/', {'title': 'a new title', 'description': 'testing', 'location': 'testing location'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
