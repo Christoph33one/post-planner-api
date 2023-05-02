bf24417d6443e4dff128eb175dffc9b22604f15d
Christoph33one <cjbrowning23@hotmail.com>
2023-05-02 12:11:59 +0000

    added delete method for users to delete a post. tested the delete function

--- travel/views.py
+++ travel/views.py
@@ -64,3 +64,10 @@ class TravelPlanPostDetail(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
