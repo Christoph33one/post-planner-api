488d4c90a87bf08986d77a434cd57c2585870c24
Christoph33one <cjbrowning23@hotmail.com>
2023-05-03 11:44:41 +0000

    added a comment serializer

--- comments/models.py
+++ comments/models.py
@@ -12,6 +12,7 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['-created_at']
