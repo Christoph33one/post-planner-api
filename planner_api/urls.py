55e787d09b159a9ba42e944fbbbde8d5f0cb88b2
Christoph33one <cjbrowning23@hotmail.com>
2023-05-03 12:09:21 +0000

    addeda generic view for the comments and tested adding a comment and to view the list of comments

--- planner_api/urls.py
+++ planner_api/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('api-auth', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('travel.urls')),
-    # path('', include('comments.ulrs')),
+    path('', include('comments.urls')),
     ]
