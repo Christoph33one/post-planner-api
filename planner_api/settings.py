10a9b9a58171b5fbdb78c97978c67ab76040e847
Christoph33one <cjbrowning23@hotmail.com>
2023-05-07 20:36:37 +0000

    added new superuser as issues with database not connecting to deployed project

--- planner_api/settings.py
+++ planner_api/settings.py
@@ -151,8 +151,8 @@ if 'DEV' in os.environ:
      }
 else:
     DATABASES = {
-         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-     }
+            'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        }
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
