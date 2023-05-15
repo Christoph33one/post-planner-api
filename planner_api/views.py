6ef73fbc9d732566ca0063521c4ef7cf8a1264ac
Christoph33one <cjbrowning23@hotmail.com>
2023-05-15 19:10:32 +0000

    cleared two lines to long errors

--- planner_api/views.py
+++ planner_api/views.py
@@ -5,11 +5,13 @@ from .settings import (
     JWT_AUTH_SECURE,
 )
 
+
 @api_view()
 def root_route(request):
     return Response({
         'message': "Welcome to my planner api"
     })
+
 
 # dj-rest-auth logout view fix
 @api_view(['POST'])
