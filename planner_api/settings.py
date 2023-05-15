1830e693bdb74c93096bfce2be99628b6625e48c
Christoph33one <cjbrowning23@hotmail.com>
2023-05-15 16:23:57 +0000

    fixed CORS error and added client orgin to evn.py file

--- planner_api/settings.py
+++ planner_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -66,6 +67,12 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = 'DEV' in os.environ
 
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-christoph33-postplanner-ts3s66e31zm.ws-eu97.gitpod.io/",
+    "https://travel-planner-api.herokuapp.com"
+]
+
 ALLOWED_HOSTS = ['localhost', 'travel-planner-api.herokuapp.com']
 
 
@@ -109,11 +116,12 @@ MIDDLEWARE = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+         os.environ.get('CLIENT_ORIGIN')
+     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
